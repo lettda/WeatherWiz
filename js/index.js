9fb2a68cd581d9dccc34c83491a94830f691b0eb
lettda <lettda@vcu.edu>
2016-12-28 16:48:04 -0500

    Background image
    Addded background image,but images don't load

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	var currentState;
 	var country;
 
-		$.getJSON("http://ip-api.com/json", function(data_init){ //access RESTFUL geo location API & set lattitude.longitude
+		$.getJSON("https://crossorigin.me/http://ip-api.com/json", function(data_init){ //access RESTFUL geo location API & set lattitude.longitude
 			lat = data_init.lat;
 			long = data_init.lon;
 			currentCity = data_init.city;
@@ -14,7 +14,7 @@ $(document).ready(function(){
 			country = data_init.country;
 					$("#cityState").append("<h2>"+currentCity + "," + currentState + "<br>" + country + "</h2>");
 
-		var api = "https://api.darksky.net/forecast/1246aa267663e22a4e428e4b20f0df5b/" + lat +"," + long;
+		// var api = "https://crossorigin.me/https://api.darksky.net/forecast/1246aa267663e22a4e428e4b20f0df5b/" + lat +"," + long;
 		//Access weather API
 		console.log(data_init);
 		
