c2a24fe00a8b458d4e311d6d4c6c2af036c756d9
lettda <lettda@vcu.edu>
2017-01-01 10:49:21 -0500

    Redo
    Deleted old styles attempting to rearrange and distribute elements

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
-	var long;
-	var lat;
-	var currentCity;
-	var currentState;
-	var country;
+	let long;
+	let lat;
+	let currentCity;
+	let currentState;
+	let country;
 
-		$.getJSON("https://crossorigin.me/http://ip-api.com/json", function(data_init){ //access RESTFUL geo location API & set lattitude.longitude
+		$.getJSON("https://crossorigin.me/http://ip-api.com/json",(data_init) =>{ //access RESTFUL geo location API & set lattitude.longitude
 			lat = data_init.lat;
 			long = data_init.lon;
 			currentCity = data_init.city;
@@ -14,14 +14,14 @@ $(document).ready(function(){
 			country = data_init.country;
 					$("#cityState").append("<h2>"+currentCity + "," + currentState + "<br>" + country + "</h2>");
 
-		// var api = "https://crossorigin.me/https://api.darksky.net/forecast/1246aa267663e22a4e428e4b20f0df5b/" + lat +"," + long;
+		// let api = "https://crossorigin.me/https://api.darksky.net/forecast/1246aa267663e22a4e428e4b20f0df5b/" + lat +"," + long;
 		//Access weather API
 		console.log(data_init);
 		
 
-			$.getJSON(api, function(data){
+			$.getJSON(api, (data) => {
 				console.log(data);
-				var weatherStatus = data.currently.icon; //get Icon from API related to current weather conditions
+				let weatherStatus = data.currently.icon; //get Icon from API related to current weather conditions
 
 				// switch(weatherStatus) {
 				// 	case:
@@ -31,12 +31,12 @@ $(document).ready(function(){
 				$(".currentConditions").append("<h4>Conditions<br>"+data.currently.summary +"</h4>");
 				$(".percipitationChance").append("<h4> Chance of Percipitation<br> " + data.currently.precipProbability +"%" + "</h4>");
 				$(".wind")	
-					$("#reveal").on('click', function(){ //click button
+					$("#reveal").on('click', () =>{ //click button
 						data.main.tempData //on click convert temperature to farenheight
 					});
 				
-				$("#tempData").hover(function(){
-					$(this).fadeToggle('slow', function(){
+				$("#tempData").hover( () => {
+					$(this).fadeToggle('slow',() => {
 
 					});
 				});
