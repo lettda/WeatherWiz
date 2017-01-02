6d8395ed6c81b12789532a9cb82d8e8f92797b92
lettda <lettda@vcu.edu>
2017-01-02 12:47:36 -0500

    updte

--- js/index.js
+++ js/index.js
@@ -16,11 +16,11 @@ $(document).ready(function(){
 
 		// let api = "https://crossorigin.me/https://api.darksky.net/forecast/1246aa267663e22a4e428e4b20f0df5b/" + lat +"," + long;
 		//Access weather API
-		console.log(data_init);
+		// console.log(data_init);
 		
 
 			$.getJSON(api, (data) => {
-				console.log(data);
+				// console.log(data);
 				let weatherStatus = data.currently.icon; //get Icon from API related to current weather conditions
 
 				// switch(weatherStatus) {
@@ -30,16 +30,29 @@ $(document).ready(function(){
 				$("#currentTemp").append("<h4 id='tempData'>Currently<br> "+data.currently.temperature+"</h4>");
 				$("#currentConditions").append("<h4>Conditions<br>"+data.currently.summary +"</h4>");
 				$("#percipitationChance").append("<h4> Chance of Percipitation<br> " + data.currently.precipProbability +"%" + "</h4>");
-				$(".wind")	
-					$("#reveal").on('click',() => { //click button
-						data.main.tempData //on click convert temperature to farenheight
-					});
+				// $(".wind")	
+				// 	$("#reveal").on('click',() => { //click button
+				// 		data.main.tempData //on click convert temperature to farenheight
+				// 	});
 				
-				$("#tempData").hover( () => {
-					$(this).fadeToggle('slow', () => {
+				// $("#tempData").hover( () => {
+				// 	$(this).fadeToggle('slow', () => {
 
-					});
-				});
+				// 	});
+				// });
+				const weekly_forecast = (argument) => {
+					
+				}
+				let day_1 = data.daily.data[1];
+				console.log(day_1);
+				let day_2 = data.daily.data[2];
+				console.log(day_2);
+				let day_3 = data.daily.data[3];
+				console.log(day_3);
+				let day_4 = data.daily.data[4];
+				console.log(day_4);
+				let day_5 = data.daily.data[5];
+				console.log(day_5);
 			});		
 		});
 });
