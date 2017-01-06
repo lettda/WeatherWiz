b002502dff4d97ef6c7ad9a5d2140f117c161358
lettda <lettda@vcu.edu>
2017-01-06 15:52:59 -0500

    mobile styling
    Mmost mobile styling complete, needs new fonts and Dynamic entry of
    weekday to corresponding forecast

--- js/index.js
+++ js/index.js
@@ -6,9 +6,7 @@ $(document).ready(function(){
 	let currentState;
 	let country;
 
-	var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-	var a = new Date('01/03/2017');
-	weekday[a.getDay()]; //get the day of the week
+
 
 		$.getJSON("https://crossorigin.me/http://ip-api.com/json", (data_init) => { //access RESTFUL geo location API & set lattitude.longitude
 			lat = data_init.lat;
@@ -30,21 +28,23 @@ $(document).ready(function(){
 				// 	case:
 				// }
 
-				$("#currentTemp").append("<h4 id='tempData'>Currently<br> "+data.currently.temperature+"</h4>");
+				$("#currentTemp").append("<h4 id='tempData'>Currently<br> "+data.currently.temperature+"&#8457</h4>");
 				$("#currentConditions").append("<h4>Conditions<br>"+data.currently.summary +"</h4>");
 				$("#percipitationChance").append("<h4> Chance of Percipitation<br> " + data.currently.precipProbability +"%" + "</h4>");
 				// 	$("#reveal").on('click',() => { //click button
 				// 		data.main.tempData //on click convert temperature to farenheight
 				// 	});
-					for (var i = 0; i < 6; i++) { //loop over the object's next 5 days
+					for (var i = 0; i < 6; i++) { //Begin loop - loop over the object's next 5 days
 						const weekly_forecast = data.daily.data;
-						$('#day_'+[i]).append("<h3>"+weekly_forecast[i].apparentTemperatureMax + "<br></h3><P>" + weekly_forecast[i].summary + "</p>");
+						$('#day_'+[i]).append("<h3>"+weekly_forecast[i].apparentTemperatureMax + "<br></h3><P>" + weekly_forecast[i].summary + "</p>"); //append a <h2> within that div containing the day and <p> containing the high and low temperature for the day
+							//Get the day of the week...
+						const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+						let a = new Date('01/03/2017');
+						let nextDay = weekday[a.getDay()+1]; //(get the day of the week)
+						alert(nextDay);
 
-						// console.log(weekly_forecast[i]);				//select the variable corresponding to the value of [i] ie. (if i = 3 variable selected should be day_3)
-															//append a <h2> within that div containing the day and <p> containing the high and low temperature for the day
+							//
 					}
-				// let day_1 = data.daily.data[1];
-				// console.log(day_1);
 			});		
 		});
 });
