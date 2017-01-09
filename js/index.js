da7a77b2dcc34d0e9b1f648351cc4294f0bb248f
lettda <lettda@vcu.edu>
2017-01-08 19:55:25 -0500

    Update need to remove last day
    Added custom fonts, App printing the 6th day into the days of the week.
    Need to remove

--- js/index.js
+++ js/index.js
@@ -22,13 +22,13 @@ $(document).ready(function(){
 		
 
 			$.getJSON(api, (data) => {
-				// console.log(data);
+				console.log(data);
 				
 				// switch(weatherStatus) {
 				// 	case:
 				// }
 
-				$("#currentTemp").append("<h4 id='tempData'>Currently<br> "+data.currently.temperature+"&#8457</h4>");
+				$("#currentTemp").append("<h4 id='tempData'>Currently<br> "+ Math.round(data.currently.temperature)+"&#8457</h4>");
 				$("#currentConditions").append("<h4>Conditions<br>"+data.currently.summary +"</h4>");
 				$("#percipitationChance").append("<h4> Chance of Percipitation<br> " + data.currently.precipProbability +"%" + "</h4>");
 				// 	$("#reveal").on('click',() => { //click button
@@ -37,23 +37,19 @@ $(document).ready(function(){
 					for (var i = 0; i < 6; i++) { //Begin loop - loop over the object's next 5 days
 						const weekly_forecast = data.daily.data;
 						let today = moment().format('D MMM, YYYY');//Format the date in the form Day / Month / Year from moment.js library
-						const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+						const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; //array for days of the week
 
-						$('#day_'+[i]).append("<h3>"+weekly_forecast[i].apparentTemperatureMax + "<br></h3><P>" + weekly_forecast[i].summary + "</p>"); //append a <h2> within that div containing the day and <p> containing the high and low temperature for the day
+						$('#day_'+[i]).append("<h3> HI: "+Math.round(weekly_forecast[i].apparentTemperatureMax) + "&#8457 LO: "+ Math.round(weekly_forecast[i].apparentTemperatureMin) +  "&#8457</h3><br><P>" + weekly_forecast[i].summary + "</p>"); //append a <h2> within that div containing the day and <p> containing the high and low temperature for the day
+							
 							//Get the day of the week...
-						let a = new Date(today); //parse current date
+						const a = new Date(today); //parse current date
 						let nextDay = weekday[a.getDay()+i]; //(get the day of the week)
-						let dayData = $("#weekDay > h2").eq(i).data('fday'); //accessthe data-attribute of each H2 within #weekday
-						$("#weekDay").append("<h2>"+weekday[a.getDay()+i+1]+"</h2>");
+						$("#weekDay").append("<h2>"+weekday[a.getDay()+i+1]+"</h2>"); //append the current day of the week in the H2
 
-						// if (dayData = [i]) {	//if the data attribute of the H2 element === i
-						// 	(weekday[i]);	//append the current day of the week in the H2
-						// }
 
-						console.log(dayData);
 
-							//
 					}
+					$('#weekDay > h4').last().remove();
 			});		
 		});
 });
